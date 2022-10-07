141ad8af5b98b9cc4db331f789ba37648791e679
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-10-07 15:02:48 +0300

    обнулил src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,16 @@
-import { TaskTitle } from 'components/TaskTitle/TaskTitle'
-import {Profile} from 'components/Profile/Profile'
-import user from '../data/user.json'
-
 export const App = () => {
   return (
-      <>
-      <TaskTitle text="1. Profile" />
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <TaskTitle text="2. Statistics" />
-      <TaskTitle text="3. FriendList" />
-      <TaskTitle text="4. TransactionHistory" />
-      </>
-    
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
