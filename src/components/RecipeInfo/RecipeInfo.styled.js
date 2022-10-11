23f176464a862f1c059ca7b813f3aed5807e2f49
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-10-11 21:44:49 +0300

    les 2

---
+++ src/components/RecipeInfo/RecipeInfo.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  alight-items: center;
+  gap: 8px;
+`;
+
+export const InfoBlock = styled.p`
+  display: flex;
+  alight-items: center;
+  gap: 4px;
+`
