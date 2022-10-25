a084b6ede5a1550261498060b354b5ce3adc4760
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-10-25 12:09:52 +0300

    full video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,51 @@
-// import { RecipeList } from './RecipeList/RecipeList';
-// import recipes from '../recipes.json';
-import { PaymentWidget } from './PaymentWidget/PaymentWidget'
+import { Component } from 'react';
+import { fetchBreeds, fetchDogByBreed } from '../api'
+import { Dog } from './Dog'
+import {BreedSelect} from './BreedSelect'
+import { GlobalStyle } from '../GlobalStyle';
 
 
+export class App extends Component {
+  state = {
+    breeds: [],
+    dog: null,
+    error: null,
+  };
 
-export const App = () => {
-  return (
-    <>
+  async componentDidMount() {
+    try {
+      const breeds = await fetchBreeds();
+      this.setState({ breeds: breeds });
+    } catch (error) {
+      this.setState({
+        error:
+          'Мы не смогли загрузить породы собачек, пожалуйста перезагрузите страницу чтобы попробовать еще раз 🥹',
+      });
+    }
+  }
 
-      <PaymentWidget />
-     
-    </>
-  );
-};
+  selectBreed = async (breedId) => {
+    try {
+      const dog = await fetchDogByBreed(breedId);
+      this.setState({ dog });
+    } catch (e) {
+      this.setState({
+        error:
+          'Упс, мы не смогли загрузить собачку 😭. Попробуйте еще раз или перезагрузите страницу 😇',
+      });
+    }
+  };
+
+  render() {
+    const { breeds, dog, error } = this.state;
+
+    return (
+      <>
+        <BreedSelect breeds={breeds} onSelect={this.selectBreed} />
+        {error && <div>{error}</div>}
+        {dog && <Dog dog={dog} />}
+        <GlobalStyle />
+      </>
+    );
+  }
+}
